3c68f708fde3106694c0eca6d40bb248e07c2115
Hardik <mhardik2003@gmail.com>
2022-08-12 17:22:45 +0530

    Added MAC changer

--- Macchangger/README.md
+++ Macchangger/README.md
@@ -1,34 +1,13 @@
-# OVERVIEW - MAC : (Media Access Control)
+To get more info : [My notion page]( https://mhardik003.notion.site/Cyber-Sec-Tools-in-Python-c89d416c8a4b41cbabed799db2639c )
+ 
+---
 
-Permanent, Phusical and Unique
-Assigned by manufacture and thus specific to each device module and therefore is used to identify computers 
-
-IP : used in internet to indetify and comunicate between devices on the internet
-
-MAC address : used within the network to identiy devices and transfer data between hthe devices
-Contains a source AMC and destination MAC
-
-## WHY CHANGE THE MAC ADDRES?
-* To make yourself anonymuos on the network
-* Impersonate other devices( thereby gaining permission assigned to them)
-* Bypassing filters
-
-lo : virtual interface created by linux
-etho0 :ethernet
-wlan0 : wifi
-
-in ifconfig
-INET : gives the IP
-netmask : netmask
-broadcast : broadcast
-ETHER :  Mac address
-
-
-
-# AUTOMATICALLY CHANGING THE MAC ADDRESS
+## Usage
 ```
-ifconfig wlan0 down     # or eth0 instead of wlan0
-ifconfig wlan0 hw ether <new MAC address> #make sure the address is of 12 characters
-ifconfig wlan0 up       # or eth0 instead of wlan0
+sudo macchanger.py -i <interface> -m <new MAC address>
 ```
+ > To see thenetwork interfaces in your computer, use the command : 
+    ```
+    ifconfig
+    ```
  
--- Macchangger/macchanger.py
+++ Macchangger/macchanger.py
@@ -1,24 +1,33 @@
 '''
--------------------------------------------------------------------------
-PROGRAM TO CHANGE THE MAC ADDRESS OF AN INTERFACE USING TERMINAL COMMANDS
--------------------------------------------------------------------------
+USAGE : sudo macchanger.py -i <interface name> -m <new MAC address>
 '''
-
+# Refer to : https://mhardik003.notion.site/Cyber-Sec-Tools-in-Python-c89d416c8a4b41cbabed799db2639c01
 
 #!/usr/bin/env python3
-
-import subprocess # using the subprocess module (to execute the system commands)
-
-''' 
-using function 'call' : runs the commands in foreground and thus waits
-for the command to  finish before moving to the next line and this is 
-very necessary for this exercise becasue thats what we want in this program
-'''
-
-#subprocess.call("ifconfig", shell=True)  
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True) #chasnge the mac address as per your need
-subprocess.call("ifconfig wlan0 up", shell=True)
-#subprocess.call("ifconfig",shell=True)
+import subprocess
+import optparse
 
 
+def get_arguements():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface",help="Interface whose MAC address you want to change")
+    parser.add_option("-m", "--mac", dest="new_mac_addr",help="New MAC address")
+    (options, arguements) = parser.parse_args()
+    if (not options.interface):
+        parser.error("[-] Please specify an interface, use --help for more info")
+    if (not options.new_mac_addr):
+        parser.error("[-] Please specify a new MAC address, use --help for more info")
+
+    return (options.interface, options.new_mac_addr)
+
+
+def change_mac(interface, new_mac_addr):
+    print("[+] Changing the MAC address for " +interface + " to " + new_mac_addr)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw","ether", new_mac_addr])
+    subprocess.call(["ifconfig", interface, "up"])
+    print("[+] Done!")
+
+
+(interface, new_mac_addr) = get_arguements()
+change_mac(interface, new_mac_addr)
